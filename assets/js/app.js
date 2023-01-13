62182211627d43393d8aff0108fc546e16920c04
Daniil <negativdaniil4@gmail.com>
2023-01-13 21:01:25 +0200

    Projec init

---
+++ assets/js/app.js
@@ -0,0 +1,21 @@
+let color = prompt('Traffic light color?', '');
+
+switch (color) {
+   case "red":
+      if (color == "red") {
+         alert('Stop!');
+      }
+      break;
+
+   case "yellow":
+      if (color == "yellow") {
+         alert('Get ready!');
+      }
+      break;
+
+   case "green":
+      if (color == "green") {
+         alert('You can go!');
+      }
+      break;
+}
